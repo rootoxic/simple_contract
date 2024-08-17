4d0f703bf75057e0ee488ffd990953f99dc69f5a
rootoxic <rootoxic@gmail.com>
2024-08-17 05:39:55 +0000

    feat: initiated the project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,14 @@
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik!!"]);
-
+  const contract = await hre.ethers.deployContract("TestToken");
   await contract.waitForDeployment();
-
-  console.log(`Swisstronik contract deployed to ${contract.target}`);
+  const deployedContract = await contract.getAddress();
+  fs.writeFileSync("contract.txt", deployedContract);
+  console.log(`Contract deployed to ${deployedContract}`);
 }
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
